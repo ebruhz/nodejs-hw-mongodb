425d1706dce9bf87876fb2aee3c4ae252625c6b0
Ebru Hızlı <delaglace1995@gmail.com>
2026-09-14 00:55:40 +0300

    fix mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+
+dotenv.config();
 
 await initMongoConnection();
 
