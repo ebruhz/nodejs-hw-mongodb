b77d15e3f4692d823dee62d31e4c8921bec7b173
Ebru Hızlı <delaglace1995@gmail.com>
2026-09-16 01:20:43 +0300

    stillnode

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+
 import {
     getAllContactsController,
     getContactByIdController,
@@ -8,18 +9,40 @@ import {
 } from '../controllers/contacts.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+
+import {
+    createContactSchema,
+    updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
+
+router.get(
+    '/contacts/:contactId',
+    isValidId,
+    ctrlWrapper(getContactByIdController),
+);
+
+router.post(
+    '/contacts',
+    validateBody(createContactSchema),
+    ctrlWrapper(createContactController),
+);
+
 router.patch(
     '/contacts/:contactId',
+    isValidId,
+    validateBody(updateContactSchema),
     ctrlWrapper(patchContactController),
 );
+
 router.delete(
     '/contacts/:contactId',
+    isValidId,
     ctrlWrapper(deleteContactController),
 );
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,44 @@
 import { Contact } from '../db/contact.js';
 
-export const getAllContacts = () => Contact.find();
+export const getAllContacts = async ({
+    page = 1,
+    perPage = 10,
+    sortBy = 'name',
+    sortOrder = 'asc',
+    type,
+    isFavourite,
+}) => {
+    const filter = {};
+
+    if (type) {
+        filter.contactType = type;
+    }
+
+    if (isFavourite !== undefined) {
+        filter.isFavourite = isFavourite === 'true';
+    }
+    const skip = (page - 1) * perPage;
+
+    const [contacts, totalItems] = await Promise.all([
+        Contact.find(filter)
+            .sort({ [sortBy]: sortOrder === 'desc' ? -1 : 1 })
+            .skip(skip)
+            .limit(perPage),
+        Contact.countDocuments(filter),
+    ]);
+
+    const totalPages = Math.ceil(totalItems / perPage);
+
+    return {
+        data: contacts,
+        page,
+        perPage,
+        totalItems,
+        totalPages,
+        hasPreviousPage: page > 1,
+        hasNextPage: page < totalPages,
+    };
+};
 
 export const getContactById = (contactId) => Contact.findById(contactId);
 
